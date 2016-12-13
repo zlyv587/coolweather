9d6e6018c8a8f9e127f97ca97874740adac3a48f
zhangliyong <505241276@qq.com>
2016-12-13 14:07:44 +0800

    ccd

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,48 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by zhangliyong on 16-12-13.
+ */
+
+public class County {
+
+    private  int id;
+
+    private  String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
